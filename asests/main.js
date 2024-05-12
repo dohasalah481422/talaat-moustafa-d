abb0846d80781da7b872d733225879f189a71643
dohasalah481422 <szaz01068@gmail.com>
2024-05-12 19:02:41 +0300

    new html

---
+++ asests/main.js
@@ -0,0 +1,50 @@
+let list = document.querySelector(".link-list");
+document.querySelector(".icon").addEventListener("click", function () {
+  list.classList.toggle("active-list");
+});
+window.addEventListener("scroll",()=>{
+    let windowscroll=scrollY;
+   if(windowscroll=1){
+    list.classList.remove("active-list");
+   };
+});
+let BtnBack=document.querySelector(".BtnBack");
+BtnBack.addEventListener("click",()=>{
+    window.scrollTo({
+        top:0,
+        behavior:"smooth"
+      });
+});
+(function() {
+  "use strict";
+
+  var carousels = function() {
+      $(".owl-carousel1").owlCarousel({
+          loop: true,
+          center: true,
+          margin: 0,
+          responsiveClass: true,
+          nav: true,
+          navText: ["<i class='fa fa-angle-left'></i>", "<i class='fa fa-angle-right'></i>"],
+          responsive: {
+              0: {
+                  items: 1,
+                  nav: true
+              },
+              680: {
+                  items: 1,
+                  nav: true,
+                  loop: false
+              },
+              1000: {
+                  items: 1,
+                  nav: true
+              }
+          }
+      });
+  };
+
+  (function($) {
+      carousels();
+  })(jQuery);
+})();
